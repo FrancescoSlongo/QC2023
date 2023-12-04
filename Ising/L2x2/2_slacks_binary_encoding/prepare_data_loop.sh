47a23327cfbdb058b7a390cc00c59cb2a0610815
Francesco Slongo <slongofrancesco@gmail.com>
2023-12-04 10:26:43 +0000

    Analysis configurations quantum

---
+++ Ising/L2x2/2_slacks_binary_encoding/prepare_data_loop.sh
@@ -0,0 +1,2 @@
+grep "]	-7.0" Outputs_2000_loop.txt | sed 's/, //g' | awk '{print $1}' | sed 's/-1/0/g' | sed 's/\[//g' | sed 's/\]//g' > configurations_loop.txt
+sort configurations_loop.txt | uniq -c > degeneracies_loop.txt
